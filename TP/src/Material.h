a8649c08489211de0399ff869881d1328287d0bb
Damien DIDIER <damien.didier@epita.fr>
2023-11-23 12:48:59 +0100

    backface culling

--- TP/src/Material.h
+++ TP/src/Material.h
@@ -3,39 +3,49 @@
 
 #include <Program.h>
 #include <Texture.h>
-
 #include <memory>
 #include <vector>
 
-namespace OM3D {
+namespace OM3D
+{
 
-enum class BlendMode {
-    None,
-    Alpha,
-};
+    enum class BlendMode
+    {
+        None,
+        Alpha,
+    };
 
-enum class DepthTestMode {
-    Standard,
-    Reversed,
-    Equal,
-    None
-};
+    enum class DepthTestMode
+    {
+        Standard,
+        Reversed,
+        Equal,
+        None
+    };
 
-class Material {
+    enum class CullMode
+    {
+        Back,
+        Front,
+        None,
+    };
 
+    class Material
+    {
     public:
         Material();
 
         void set_program(std::shared_ptr<Program> prog);
         void set_blend_mode(BlendMode blend);
         void set_depth_test_mode(DepthTestMode depth);
+        void set_cull_mode(CullMode cull);
         void set_texture(u32 slot, std::shared_ptr<Texture> tex);
 
-        template<typename... Args>
-        void set_uniform(Args&&... args) {
+        template <typename... Args>
+        void set_uniform(Args &&...args)
+        {
             _program->set_uniform(FWD(args)...);
         }
-
 
         void bind() const;
 
@@ -43,16 +53,15 @@ class Material {
         static Material textured_material();
         static Material textured_normal_mapped_material();
 
-
     private:
         std::shared_ptr<Program> _program;
         std::vector<std::pair<u32, std::shared_ptr<Texture>>> _textures;
 
         BlendMode _blend_mode = BlendMode::None;
         DepthTestMode _depth_test_mode = DepthTestMode::Standard;
+        CullMode _cull_mode = CullMode::Back;
+    };
 
-};
-
-}
+} // namespace OM3D
 
 #endif // MATERIAL_H
