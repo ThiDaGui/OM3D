34fd47a43475d05870316ea9ecf6aea2e9b8bfce
jules cassegrain <jules.cassegrain@epita.fr>
2024-01-24 19:42:57 +0100

    removed useless print for submission

--- TP/src/Scene_loader.cpp
+++ TP/src/Scene_loader.cpp
@@ -13,7 +13,7 @@
 namespace OM3D
 {
 
-bool display_gltf_loading_warnings = true;
+bool display_gltf_loading_warnings = false;
 
 static size_t component_count(int type)
 {
@@ -507,19 +507,6 @@ Result<std::unique_ptr<Scene>> Scene::from_gltf(const std::string &file_name)
                         gltf.materials[prim.material].alphaMode;
                     const auto &alpha_cutoff =
                         gltf.materials[prim.material].alphaCutoff;
-                    std::cout << "alpha_info: " << alpha_info
-                              << " for material name: "
-                              << gltf.materials[prim.material].name
-                              << "with index: " << prim.material << std::endl;
-                    std::cout << "alpha_cutoff: " << alpha_cutoff
-                              << " for material name: "
-                              << gltf.materials[prim.material].name
-                              << "with index: " << prim.material << std::endl;
-                    std::cout << "base_color: " << base_color[0] << " "
-                              << base_color[1] << " " << base_color[2] << " "
-                              << base_color[3] << " for material name: "
-                              << gltf.materials[prim.material].name
-                              << "with index: " << prim.material << std::endl;
                     auto load_texture =
                         [&](auto texture_info,
                             bool as_sRGB) -> std::shared_ptr<Texture> {
@@ -580,7 +567,7 @@ Result<std::unique_ptr<Scene>> Scene::from_gltf(const std::string &file_name)
                         else if (alpha_info == "MASK")
                             mat = std::make_shared<Material>(
                                 Material::textured_masked_material(
-                                    alpha_cutoff));
+                                    (float)alpha_cutoff));
                         else
                             mat = std::make_shared<Material>(
                                 Material::textured_material());
@@ -596,7 +583,7 @@ Result<std::unique_ptr<Scene>> Scene::from_gltf(const std::string &file_name)
                             mat = std::make_shared<Material>(
                                 Material::
                                     textured_normal_mapped_masked_material(
-                                        alpha_cutoff));
+                                        (float)alpha_cutoff));
                         else
                             mat = std::make_shared<Material>(
                                 Material::textured_normal_mapped_material());
@@ -651,64 +638,6 @@ Result<std::unique_ptr<Scene>> Scene::from_gltf(const std::string &file_name)
         }
         scene->add_light(light);
     }
-
-    // print info about the scene we just loaded
-    std::cout << "Scene info:" << std::endl;
-    std::cout << "    " << scene->objects().size() << " objects" << std::endl;
-    std::cout << "    " << scene->point_lights().size() << " point lights"
-              << std::endl;
-    std::cout << "    " << textures.size() << " textures" << std::endl;
-    for (auto [id, texture] : textures)
-    {
-        std::cout << "        " << id << ": " << texture->size().x << "x"
-                  << texture->size().y << std::endl;
-    }
-    for (auto [id, material] : materials)
-    {
-        auto blend_mode = material->blend_mode();
-        if (blend_mode == BlendMode::None)
-        {
-            std::cout << "        " << id << ": "
-                      << "BlendMode::None" << std::endl;
-        }
-        else if (blend_mode == BlendMode::Alpha)
-        {
-            std::cout << "        " << id << ": "
-                      << "BlendMode::Alpha" << std::endl;
-        }
-        else if (blend_mode == BlendMode::Additive)
-        {
-            std::cout << "        " << id << ": "
-                      << "BlendMode::Additive" << std::endl;
-        }
-        else
-        {
-            std::cout << "        " << id << ": "
-                      << "BlendMode::Unknown" << std::endl;
-        }
-        auto alpha_mode = material->alpha_mode();
-        if (alpha_mode == AlphaMode::Opaque)
-        {
-            std::cout << "        " << id << ": "
-                      << "AlphaMode::Opaque" << std::endl;
-        }
-        else if (alpha_mode == AlphaMode::Mask)
-        {
-            std::cout << "        " << id << ": "
-                      << "AlphaMode::Mask" << std::endl;
-        }
-        else if (alpha_mode == AlphaMode::Blend)
-        {
-            std::cout << "        " << id << ": "
-                      << "AlphaMode::Blend" << std::endl;
-        }
-        else
-        {
-            std::cout << "        " << id << ": "
-                      << "AlphaMode::Unknown" << std::endl;
-        }
-    }
-
     return { true, std::move(scene) };
 }
 
